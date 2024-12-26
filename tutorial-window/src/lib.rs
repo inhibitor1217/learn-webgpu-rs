1cb72a2eb24f0a6541850fa3fef2caf1bba088a2
dugi <dugi@channel.io>
2024-12-26 16:13:27 +0900

    refactor: reorganize

--- tutorial-window/src/lib.rs
+++ tutorial-window/src/lib.rs
@@ -8,60 +8,86 @@ use winit::{
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
+enum AppState {
+    Uninitialized,
+    Initialized(App),
 }
 
-impl ApplicationHandler for App {
+impl AppState {
+    fn init(&mut self, event_loop: &ActiveEventLoop) {
+        match self {
+            AppState::Initialized(_) => panic!("app already initialized"),
+            AppState::Uninitialized => {
+                *self = AppState::Initialized(App::new(event_loop));
+            }
+        }
+    }
+}
+
+impl ApplicationHandler for AppState {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes = Window::default_attributes().with_title("Tutorial Window");
-        self.window = Some(event_loop.create_window(window_attributes).unwrap());
-
-        #[cfg(target_arch = "wasm32")]
-        {
-            use winit::dpi::PhysicalSize;
-
-            let window = self.window.as_ref().unwrap();
-            let _ = window.request_inner_size(PhysicalSize::new(800, 600));
-
-            use winit::platform::web::WindowExtWebSys;
-
-            web_sys::window()
-                .and_then(|win| win.document())
-                .and_then(|doc| {
-                    let dst = doc.get_element_by_id("app")?;
-                    let canvas = web_sys::Element::from(window.canvas()?);
-                    dst.append_child(&canvas).ok()?;
-                    Some(())
-                })
-                .expect("failed to append canvas to DOM");
+        match self {
+            AppState::Initialized(_) => (), // no-op for now
+            AppState::Uninitialized => self.init(event_loop),
         }
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        let app = match self {
+            AppState::Initialized(app) => app,
+            AppState::Uninitialized => return,
+        };
+
         match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::RedrawRequested => self.window.as_ref().unwrap().request_redraw(),
+            WindowEvent::CloseRequested | WindowEvent::Destroyed => event_loop.exit(),
+            WindowEvent::RedrawRequested => app.window.request_redraw(),
             _ => (),
         }
     }
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
-pub fn run() {
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-          std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-          console_log::init_with_level(log::Level::Warn).unwrap();
-        } else {
-          env_logger::init();
-        }
+struct App {
+    window: Window,
+}
+
+impl App {
+    fn run() {
+        let event_loop = EventLoop::new().unwrap();
+        event_loop.set_control_flow(ControlFlow::Poll);
+
+        let mut app_state = AppState::Uninitialized;
+        event_loop.run_app(&mut app_state).unwrap();
     }
 
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
+    fn new(event_loop: &ActiveEventLoop) -> Self {
+        let window_attrs = Window::default_attributes().with_title("Tutorial Window");
+        let window = event_loop.create_window(window_attrs).unwrap();
 
-    let mut app = App::default();
-    event_loop.run_app(&mut app).unwrap();
+        #[cfg(target_arch = "wasm32")]
+        {
+            // connect js console logs
+            std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+            console_log::init_with_level(log::Level::Warn).unwrap();
+
+            // connect canvas
+            use winit::platform::web::WindowExtWebSys;
+
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| {
+                    let root = doc.get_element_by_id("root").unwrap();
+                    let canvas = web_sys::Element::from(window.canvas()?);
+                    root.append_child(&canvas).unwrap();
+                    Some(())
+                })
+                .unwrap();
+        }
+
+        Self { window }
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    App::run();
 }
