84be823c385b8e744d3f96da55c9e5061a439576
dugi <dugi@channel.io>
2024-12-26 14:47:36 +0900

    build: support wasm32 target

---
+++ tutorial-window/src/lib.rs
@@ -0,0 +1,67 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    window::{Window, WindowId},
+};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes().with_title("Tutorial Window");
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            use winit::dpi::PhysicalSize;
+
+            let window = self.window.as_ref().unwrap();
+            let _ = window.request_inner_size(PhysicalSize::new(800, 600));
+
+            use winit::platform::web::WindowExtWebSys;
+
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| {
+                    let dst = doc.get_element_by_id("app")?;
+                    let canvas = web_sys::Element::from(window.canvas()?);
+                    dst.append_child(&canvas).ok()?;
+                    Some(())
+                })
+                .expect("failed to append canvas to DOM");
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::RedrawRequested => self.window.as_ref().unwrap().request_redraw(),
+            _ => (),
+        }
+    }
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn run() {
+    cfg_if::cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+          std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+          console_log::init_with_level(log::Level::Warn).unwrap();
+        } else {
+          env_logger::init();
+        }
+    }
+
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
+}
